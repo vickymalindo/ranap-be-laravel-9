478f01796747a87b7b594538847c1f82f89d8aa1
vickymalindo <vicky.malindo@gmail.com>
2024-04-30 16:37:21 +0700

    feat: change link navbar

--- resources/views/partial/navbar-ranap.blade.php
+++ resources/views/partial/navbar-ranap.blade.php
@@ -9,7 +9,7 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/ranap">Ranap</a>
+            <a class="nav-link active" aria-current="page" href="/ranap">Inpatient Monitoring</a>
           </li>
         </ul>
     </div>
